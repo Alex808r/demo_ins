b82be8afa493b5649373a948f5df370f68bb98bd
Alex808r <fromxlam@gmail.com>
2021-07-20 20:44:10 +0300

    [RC-3] Add model Article add routes to Article

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "home#index"
+
+  resources :articles
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_160542) do
+ActiveRecord::Schema.define(version: 2021_07_20_174009) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
