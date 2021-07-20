5faa9fdcb18a5bde20af6910dd0126a0320dd079
Alex808r <fromxlam@gmail.com>
2021-07-20 19:48:47 +0300

    [RC-2] Add devise views to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
   def hello
      render html: "Hello, this is demo Instagram"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  #get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  #root 'application#hello'
 
-  #root to: "home#index"
+  root to: "home#index"
 end
