34c3feda663ff47f9c0519dd4d1715d2b3d14c40
Alex808r <fromxlam@gmail.com>
2021-07-21 10:04:49 +0300

    [RC-3] Add edit and update to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,23 @@
 class ArticlesController < ApplicationController
+
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article # вызовет метод show и переведет пользователя на новую страницу
+    else
+      render 'edit' # перезагрузка страницы если не прошла валидация
+    end
+  end
+
 
   def create
     @article = Article.new(article_params)
