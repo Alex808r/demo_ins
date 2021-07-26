50da997a42e120519db78bf3b691eadf1577fdc2
Alex808r <fromxlam@gmail.com>
2021-07-26 12:59:27 +0300

    [RC-4] add action uptade and destroy to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,30 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_update_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_update_params
+    params.require(:user).permit(:username)
+  end
+
 
 end
