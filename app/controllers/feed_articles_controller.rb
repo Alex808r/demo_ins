44a37bda8da7fa850908802729dbbf9858daf4aa
Alex808r <fromxlam@gmail.com>
2021-08-16 10:24:05 +0300

    Fix pluck style guide

--- app/controllers/feed_articles_controller.rb
+++ app/controllers/feed_articles_controller.rb
@@ -3,7 +3,8 @@ class FeedArticlesController < ApplicationController
 
   def index
     if current_user.followings.exists?
-      @articles = Article.where(user_id: current_user.followings.pluck(:id)).order(created_at: :desc)
+      # @articles = Article.where(user_id: current_user.followings.pluck(:id)).order(created_at: :desc) аналогичная запись более правильная по style guide
+      @articles = Article.where(user_id: current_user.followings.ids).order(created_at: :desc)
     else
       Article.none
     end
